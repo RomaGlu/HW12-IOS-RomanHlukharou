a5c54747175a8913be3176cb98f6cb9b05520c75
RomaGlu <roma.glu.69@gmail.com>
2023-06-20 11:09:37 +0300

    Initial Commit

---
+++ HW12-IOS-RomanHlukharou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12-IOS-RomanHlukharou
+//
+//  Created by Роман Глухарев on 20/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
